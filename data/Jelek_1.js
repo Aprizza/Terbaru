9956d79ee3ade842c382e8edb9fe4439bf18e0a1
Aprizza <56188101+Aprizza@users.noreply.github.com>
2020-12-01 23:58:59 +0700

    Add files via upload

---
+++ data/Jelek_1.js
@@ -0,0 +1,8 @@
+var json_Jelek_1 = {
+"type": "FeatureCollection",
+"name": "Jelek_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.388832659256991, -6.968790165625141 ] } }
+]
+}
